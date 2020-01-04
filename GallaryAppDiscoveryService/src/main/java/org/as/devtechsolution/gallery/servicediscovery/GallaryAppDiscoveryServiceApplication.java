66d7c416f99277fc621cc8f769fb1870ffa660c8
devtechsolution <srivastvadevzone@gmail.com>
2020-01-04 20:17:59 +0530

    Initial Commit for Service Discovery Implementation for App.

---
+++ GallaryAppDiscoveryService/src/main/java/org/as/devtechsolution/gallery/servicediscovery/GallaryAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.gallery.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallaryAppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallaryAppDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ GallaryAppDiscoveryService/src/test/java/org/as/devtechsolution/gallery/servicediscovery/GallaryAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.gallery.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallaryAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
