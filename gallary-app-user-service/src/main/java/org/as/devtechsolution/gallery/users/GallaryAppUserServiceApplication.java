ddc6c9fbd0b02a1f036a7457099991d0b23dc3f0
devtechsolution <srivastvadevzone@gmail.com>
2020-01-04 21:12:02 +0530

    First Commit of User Service for Registraion And Authentication

---
+++ gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/GallaryAppUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.gallery.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GallaryAppUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GallaryAppUserServiceApplication.class, args);
+	}
+
+}
---
+++ gallary-app-user-service/src/test/java/org/as/devtechsolution/gallery/users/GallaryAppUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.gallery.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallaryAppUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
