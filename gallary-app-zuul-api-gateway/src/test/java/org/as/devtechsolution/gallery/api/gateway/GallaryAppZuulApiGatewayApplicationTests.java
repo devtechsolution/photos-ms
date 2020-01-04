f8f8461bc5c7993556de6c50e5d85b897fa048a5
devtechsolution <srivastvadevzone@gmail.com>
2020-01-05 03:04:46 +0530

    Firdt Commit Zuul API Gateway with Ribbon Load Balancer

---
+++ gallary-app-zuul-api-gateway/src/test/java/org/as/devtechsolution/gallery/api/gateway/GallaryAppZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.gallery.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallaryAppZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
