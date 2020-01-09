a0eb9c2d9dd39579bf1621b1bd8562711c1b45f1
devtechsolution <srivastvadevzone@gmail.com>
2020-01-09 08:42:01 +0530

    Secure User-ms

---
+++ gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/security/WebSecurity.java
@@ -0,0 +1,24 @@
+package org.as.devtechsolution.gallery.users.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private Environment env;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		//http.authorizeRequests().antMatchers("/users/**").permitAll();
+		// To Allow only IP address of Zuul API Gateway instead of users
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"));
+		
+		http.headers().frameOptions().disable();//Disable to show h2-console
+	}
+
+}
--- gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/service/UserServiceImpl.java
+++ gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/service/UserServiceImpl.java
@@ -8,22 +8,26 @@ import org.as.devtechsolution.gallery.users.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository UserRepository;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
   
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		ModelMapper modelMapper= new ModelMapper(); 
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
 		User user = modelMapper.map(userDetails, User.class);
-		user.setEncryptedPassword("Test");
+		//user.setEncryptedPassword("Test");
 		UserRepository.save(user);
 		UserDto dto= modelMapper.map(user, UserDto.class);
 	
