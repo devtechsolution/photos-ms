e0a6505479c50929dc5fd46fc737505e88fb3449
devtechsolution <srivastvadevzone@gmail.com>
2020-02-20 19:38:46 +0530

    Zuul API Gateway Security

--- gallary-app-zuul-api-gateway/src/main/java/org/as/devtechsolution/gallery/api/gateway/GallaryAppZuulApiGatewayApplication.java
+++ gallary-app-zuul-api-gateway/src/main/java/org/as/devtechsolution/gallery/api/gateway/GallaryAppZuulApiGatewayApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@ComponentScan(basePackages= {"org.as.devtechsolution.gallery.api.gateway"})
 public class GallaryAppZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
