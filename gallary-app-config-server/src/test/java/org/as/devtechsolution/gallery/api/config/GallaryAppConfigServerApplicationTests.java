ddffed5bcf341a29e83f901b7eb886b8414156a0
devtechsolution <srivastvadevzone@gmail.com>
2020-02-21 02:37:39 +0530

    Spring Cloud Config Server without Cloud Bus remaining file

---
+++ gallary-app-config-server/src/test/java/org/as/devtechsolution/gallery/api/config/GallaryAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.gallery.api.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallaryAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
