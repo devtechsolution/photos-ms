569182d91e9a5daeb4f9eb7101ae239725220ece
devtechsolution <srivastvadevzone@gmail.com>
2020-03-06 16:56:14 +0530

    Added feign logger in user-ms

--- gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/GallaryAppUserServiceApplication.java
+++ gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/GallaryAppUserServiceApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import feign.Logger;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -28,6 +30,12 @@ public class GallaryAppUserServiceApplication {
 	public RestTemplate getRestTemplate()
 	{
 		return new RestTemplate();
+	}
+	
+	@Bean
+	Logger.Level feignLoggerLevel(){
+		
+		return Logger.Level.FULL;
 	}
 
 }
