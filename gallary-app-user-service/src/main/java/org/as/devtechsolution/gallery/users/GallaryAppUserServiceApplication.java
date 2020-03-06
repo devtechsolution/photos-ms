2edac0057edc4249234e333d3364e6a7293ba5b1
devtechsolution <srivastvadevzone@gmail.com>
2020-03-06 21:30:13 +0530

    Implementing Hystrix and fallback for user-ms

--- gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/GallaryAppUserServiceApplication.java
+++ gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/GallaryAppUserServiceApplication.java
@@ -3,6 +3,7 @@ package org.as.devtechsolution.gallery.users;
 import org.as.devtechsolution.gallery.users.exception.FeignErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,11 +11,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+
 import feign.Logger;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class GallaryAppUserServiceApplication {
 
 	public static void main(String[] args) {
--- gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/client/AlbumServiceClient.java
+++ gallary-app-user-service/src/main/java/org/as/devtechsolution/gallery/users/client/AlbumServiceClient.java
@@ -1,16 +1,28 @@
 package org.as.devtechsolution.gallery.users.client;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.as.devtechsolution.gallery.users.bean.AlbumResBean;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="albums-ms")
+@FeignClient(name="albums-ms", fallback=AlbumFallback.class)
 public interface AlbumServiceClient {
 	
-	@GetMapping("/users/{id}/albumsss")
+	@GetMapping("/users/{id}/albums")
 	public List<AlbumResBean> getAlbums(@PathVariable String id) ;
 
 }
+
+@Component
+class AlbumFallback implements AlbumServiceClient{
+
+	@Override
+	public List<AlbumResBean> getAlbums(String id) {
+		return new ArrayList<>();
+	}
+	
+} 
